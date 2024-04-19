95ab83a4fca7d637924a56f435a7ad80bc96ac2e
kajugadaniels <kajugadaniels@gmail.com>
2024-04-19 17:35:47 +0200

    Initialize sidebar panel - contactInfo

--- frontend/src/components/sidebar-panel/ContactInfo.js
+++ frontend/src/components/sidebar-panel/ContactInfo.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
 
 const ContactInfo = () => {
-    return (
-        <div>ContactInfo</div>
-    )
-}
+    const contactInfo = [
+        {
+            id: 1,
+            title: "Total Free Customer Care",
+            phone: "+(250) 788 888 888",
+            phoneHref: "tel:+250788888888",
+        },
+        {
+            id: 2,
+            title: "Need Live Support?",
+            email: "test@test.com",
+            emailHref: "mailto:test@test.com",
+        },
+    ];
 
-export default ContactInfo
+    return (
+        <>
+            {contactInfo.map((info) => (
+                <div className="col-auto" key={info.id}>
+                    <div className="contact-info">
+                        <p className="info-title dark-color">{info.title}</p>
+                        {info.phone && (
+                            <h6 className="info-phone dark-color">
+                                <a href={info.phoneHref}>{info.phone}</a>
+                            </h6>
+                        )}
+                        {info.email && (
+                            <h6 className="info-mail dark-color">
+                                <a href={info.emailHref}>{info.email}</a>
+                            </h6>
+                        )}
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+};
+
+export default ContactInfo;
