9f876fe20e9ba4989977b3ee334dd876ffb08643
kajugadaniels <kajugadaniels@gmail.com>
2024-04-19 17:50:35 +0200

    Initialize mobile menu - pro sidebar content

--- frontend/src/components/mobile-menu/ProSidebarContent.js
+++ frontend/src/components/mobile-menu/ProSidebarContent.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { isParentActive } from "@/utils/isMenuActive";
+import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 
 const ProSidebarContent = () => {
-    return (
-        <div>ProSidebarContent</div>
-    )
-}
+    const path = usePathname();
 
-export default ProSidebarContent
+
+
+    return (
+        <Sidebar width="100%" backgroundColor="#fff" className="my-custom-class">
+            <Menu>
+                <SubMenu className="" label="Home" />
+                <SubMenu className="" label="Property" />
+                <SubMenu className="" label="Agents" />
+                <SubMenu className="" label="Blog" />
+                <SubMenu className="" label="Contact" />
+            </Menu>
+        </Sidebar>
+    );
+};
+
+export default ProSidebarContent;
