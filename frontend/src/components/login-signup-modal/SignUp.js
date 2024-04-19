77486ea54b4ed370c8bc8d9cf5f76c3fa29ed9ee
kajugadaniels <kajugadaniels@gmail.com>
2024-04-19 17:24:35 +0200

    Design signup model

--- frontend/src/components/login-signup-modal/SignUp.js
+++ frontend/src/components/login-signup-modal/SignUp.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import Link from "next/link";
 
 const SignUp = () => {
     return (
-        <div>SignUp</div>
-    )
-}
+        <form className="form-style1">
+            <div className="mb25">
+                <label className="form-label fw600 dark-color">Username</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Username"
+                    required
+                />
+            </div>
 
-export default SignUp
+            <div className="mb25">
+                <label className="form-label fw600 dark-color">Email</label>
+                <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Enter Email"
+                    required
+                />
+            </div>
+
+            <div className="mb20">
+                <label className="form-label fw600 dark-color">Password</label>
+                <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Enter Password"
+                    required
+                />
+            </div>
+
+            <div className="d-grid mb20">
+                <button className="ud-btn btn-thm" type="submit">
+                    Create account <i className="fal fa-arrow-right-long" />
+                </button>
+            </div>
+
+            <p className="dark-color text-center mb0 mt10">
+                Already Have an Account?{" "}
+                <Link className="dark-color fw600" href="/login">
+                    Login
+                </Link>
+            </p>
+        </form>
+    );
+};
+
+export default SignUp;
