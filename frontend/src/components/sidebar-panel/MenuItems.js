8cc63400a2ecbdc5895e82dc4b8ad940e4fb22a3
kajugadaniels <kajugadaniels@gmail.com>
2024-04-19 17:36:45 +0200

    Initialize sidebar panel - menuItems

--- frontend/src/components/sidebar-panel/MenuItems.js
+++ frontend/src/components/sidebar-panel/MenuItems.js
@@ -1,9 +1,25 @@
-import React from 'react'
-
 const MenuItems = () => {
-    return (
-        <div>MenuItems</div>
-    )
-}
+    const menuItems = [
+        { id: 1, title: "Apartments" },
+        { id: 2, title: "Bungalow" },
+        { id: 3, title: "Houses" },
+        { id: 4, title: "Loft" },
+        { id: 5, title: "Office" },
+        { id: 6, title: "Townhome" },
+        { id: 7, title: "Villa" },
+    ];
 
-export default MenuItems
+    return (
+        <ul className="navbar-nav">
+            {menuItems.map((item) => (
+                <li className="nav-item" key={item.id}>
+                    <a className="nav-link" href="#" role="button">
+                        {item.title}
+                    </a>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default MenuItems;
