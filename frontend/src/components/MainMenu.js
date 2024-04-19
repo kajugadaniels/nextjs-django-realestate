a150016086b085be98410d7158651eb263874945
kajugadaniels <kajugadaniels@gmail.com>
2024-04-19 17:02:12 +0200

    Stylize MainMenu from header

--- frontend/src/components/MainMenu.js
+++ frontend/src/components/MainMenu.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const MainMenu = () => {
+    const pathname = usePathname();
+
+    const handleActive = (link) => {
+        if (link.split("/")[1] == pathname.split("/")[1]) {
+            return "menuActive";
+        }
+    };
+
     return (
-        <div>MainMenu</div>
+        <ul className="ace-responsive-menu">
+            <li className="visible_list dropitem">
+                <a className="list-item" href="#">
+                    <span className={"home" ? "title menuActive" : "title"}>
+                        Home
+                    </span>
+                </a>
+            </li>
+            <li className="visible_list dropitem">
+                <a className="list-item" href="#">
+                    <span className="">
+                        Property
+                    </span>
+                </a>
+            </li>
+            <li className="visible_list dropitem">
+                <a className="list-item" href="#">
+                    <span className="">
+                        Agents
+                    </span>
+                </a>
+            </li>
+            <li className="visible_list dropitem">
+                <a className="list-item" href="#">
+                    <span className="">
+                        Blog
+                    </span>
+                </a>
+            </li>
+            <li className="visible_list dropitem">
+                <a className="list-item" href="#">
+                    <span className="">
+                        Contact
+                    </span>
+                </a>
+            </li>
+        </ul>
     )
 }
 
