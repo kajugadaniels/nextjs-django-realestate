1b98b2223951eacd890b50071fb64b156180e026
kajugadaniels <kajugadaniels@gmail.com>
2024-04-19 17:55:33 +0200

    Designing hero

--- frontend/src/app/(home)/page.js
+++ frontend/src/app/(home)/page.js
@@ -1,4 +1,6 @@
 import Header from "@/components/Header";
+import Hero from "@/components/hero";
+import HeroSlide from "@/components/hero/HeroSlide";
 import MobileMenu from "@/components/mobile-menu";
 
 export const metadata = {
@@ -12,7 +14,26 @@ const Home = () => {
 
             <MobileMenu />
 
-            Home
+            <section className="home-banner-style7 pt0 pb30 bgc-white">
+                <div className="home-style7 maxw1600 bdrs24 position-relative mx-auto mx10-lg">
+                    <div className="container-fluid">
+                        <div className="row align-items-center">
+                            <div className="col-xl-6">
+                                <Hero />
+                            </div>
+                            {/* End .col-xl-8 */}
+
+                            <div className="col-xl-6">
+                                <div className="home7-main-slider">
+                                    <HeroSlide />
+                                </div>
+                            </div>
+                            {/* End .col-xl-4 */}
+                        </div>
+                    </div>
+                    {/* End .container */}
+                </div>
+            </section>
         </div>
     )
 }
